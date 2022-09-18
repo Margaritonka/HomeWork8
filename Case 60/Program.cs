89742fb0b39feb6820e3c228a0a45c0a139f51c6
Margarita <bano4kaa@mail.ru>
2022-09-18 16:16:42 +0300

    case 60 done

--- Case 60/Program.cs
+++ Case 60/Program.cs
@@ -5,3 +5,57 @@
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+
+int [,,] FillArray(int m, int n, int l)
+{
+    int [,,] array = new int [m, n, l];
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+               array[i, j, k] = new Random().Next(10, 30); 
+            }
+        }
+    }
+    return array;
+}
+
+// void PrintArray(int [,,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matr.GetLength(2); k++)
+//             {
+//             Console.Write(matr[i, j, k] + " ");
+//             }
+//         Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+void PrintArrayIndex(int [,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+            Console.Write(matr[i, j, k] + ($" {(i, j, k)}  "));
+            }
+        Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int [,,] exy = FillArray(3,3,3);
+// PrintArray(exy);
+PrintArrayIndex(exy);
